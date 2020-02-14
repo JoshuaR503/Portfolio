084810d3952948355a9960320cf0418fd87c851f
Joshua Garcia <josshuagarciia000@gmail.com>
2020-02-13 19:38:56 -0800

    fixed directory

---
+++ controllers/home/home.controller.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Searches all collections.
+ * @param request
+ * @param response
+ */
+var renderHomeScreen = function (request, response) {
+    response.render('home/home');
+};
+exports.renderHomeScreen = renderHomeScreen;
