17d029f0583f1a835b7d24b3b74d71e28878b37e
Joshua Garcia <josshuagarciia000@gmail.com>
2020-02-13 19:39:45 -0800

    removed js files

--- routes/home.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var home_controller_1 = require("../controllers/home/home.controller");
-var homeRouter = express_1.Router();
-homeRouter.get('/', home_controller_1.renderHomeScreen);
-exports.default = homeRouter;
